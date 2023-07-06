208c8ab091b1d32bbbe5bd1f3f7a3dd986430cc9
mohanpatrick <121981571+mohanpatrick@users.noreply.github.com>
2023-07-06 15:43:57 -0700

    Add files via upload

--- update_mfl_sfb13.R
+++ update_mfl_sfb13.R
@@ -16,7 +16,7 @@ options(dplyr.summarise.inform = FALSE,
 #GITHUB_PAT <- Sys.setenv("GITHUB_PAT")
 #Sys.setenv("MFL_CLIENT")
 mfl_client <- Sys.getenv(c("MFL_CLIENT"))
-cli::cli_alert("Client ID: {MFL_CLIENT}")
+cli::cli_alert("Client ID: {mfl_client}")
 
  mfl_leagues <- mfl_getendpoint(mfl_connect(2022),"leagueSearch", user_agent="MFLRCLIENT", SEARCH="#SFB12") |>
    purrr::pluck("content","leagues","league") |>
